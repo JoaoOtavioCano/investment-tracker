4f18fe0a5aa959dd1191bf3f1fb0e4e302293f7c
João Otávio Cano <joaootaviocano@gmail.com>
2023-11-23 15:45:21 -0300

    started to validate inpus and calculate the total of the transaction

--- frontend/javascript/new-transaction.js
+++ frontend/javascript/new-transaction.js
@@ -11,3 +11,43 @@ function clickSell(){
 
     document.getElementById("buy/sell").value = 'sell';
 }
+
+function validateFormInputs(){
+    inputs = document.getElementsByTagName("input");
+
+    for(let i = 0; i < inputs.length; i++){
+        if(inputs[i].value == ""){
+            alert("All the fields must be filled")
+            return false;
+        }
+    }
+
+    return true
+}
+
+function submitForm(){
+    if(!validateFormInputs()){
+        return false;
+    }
+}
+
+function calculateTotal(){
+    const price = document.getElementById("price").value;
+    const quantity = document.getElementById("quantity").value;
+
+    const total = price * quantity;
+
+    document.getElementById("total").textContent = "TOTAL: $" + total.toString();
+}
+
+function recalculateTotal(){
+    document.getElementById("price").addEventListener("input", () => {
+        calculateTotal();
+    })
+    
+    document.getElementById("quantity").addEventListener("input", () => {
+        calculateTotal();
+    })
+}
+
+recalculateTotal()
