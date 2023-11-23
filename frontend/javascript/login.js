bba3c0f53295dd7ae14cc61c0ed5f65a156f1cb6
João Otávio Cano <joaootaviocano@gmail.com>
2023-11-23 02:03:12 -0300

    started login.js

---
+++ frontend/javascript/login.js
@@ -0,0 +1,26 @@
+function validateFormInputs(){
+    const email = document.forms["login"]["email"].value;
+    const password = document.forms["login"]["password"].value;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(email == "" || password == "")
+    {
+        alert("Both fields must be filled");
+        return false;
+    }else if(!emailRegex.test(email)){
+        alert("Type a valid email");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function submitForm(){
+
+    if(!validateFormInputs()){
+        return false;
+    }
+
+
+
+}
