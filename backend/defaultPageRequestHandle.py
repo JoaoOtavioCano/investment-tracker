f2e71e6c059a2eb97075d1c7f11ae1c18a0d0d18
João Otávio Cano <joaootaviocano@gmail.com>
2023-11-24 21:04:37 -0300

    fix defautlPageRequestHandler for multiple css files

--- backend/defaultPageRequestHandle.py
+++ backend/defaultPageRequestHandle.py
@@ -1,6 +1,18 @@
 from pages import Pages
 import re
 
+def checkMultipleFiles(path, list_of_files_path):
+    counter = 0
+
+    if type(list_of_files_path) == str and path in list_of_files_path:
+        return -1
+
+    for file_path in list_of_files_path: 
+        if path in file_path:
+            return counter
+        counter = counter + 1
+    return -2
+
 class DefaultPageRequestHandle:
     def __init__(self, request_handler):
         self.request_handler = request_handler
@@ -21,9 +33,15 @@ class DefaultPageRequestHandle:
                 self.request_handler.end_headers()
                 self.request_handler.wfile.write(html_content)
                 break
-            elif self.request_handler.path in Pages.pages[page]["css"]:
-                with open(Pages.pages[page]["css"], 'rb') as css_file:
-                    css_content = css_file.read()
+            elif checkMultipleFiles(self.request_handler.path ,Pages.pages[page]["css"]) >= -1:
+                index = checkMultipleFiles(self.request_handler.path ,Pages.pages[page]["css"])
+
+                if index == -1:
+                    with open(Pages.pages[page]["css"], 'rb') as css_file:
+                        css_content = css_file.read()
+                else:
+                    with open(Pages.pages[page]["css"][index], 'rb') as css_file:
+                        css_content = css_file.read()
                 
                 self.request_handler.send_response(200, "OK")
                 self.request_handler.send_header('Content-type', 'text/css')
