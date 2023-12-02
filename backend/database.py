6125867eb352b0844a6b9f44b428c8bba002cc39
João Otávio Cano <joaootaviocano@gmail.com>
2023-12-02 15:06:23 -0300

    get every asset of a user from the db

---
+++ backend/database.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+class Database:
+
+    def __init__(self):
+        self.__db__ = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="91246810",
+            database="testdatabase"
+        )
+
+        self.__mycursor__ = self.__db__.cursor()
+    
+    def getAssets(self, user):
+
+        sql_querry = sql_querry = ("SELECT type, Stocks.name, quantity, cost FROM User JOIN "
+            "Assets ON User.userID = Assets.userID "
+            "JOIN Stocks ON Assets.userID = Stocks.userID AND Assets.name = Stocks.name "
+            f"WHERE User.userID = {user}")
+        
+        self.__mycursor__.execute(sql_querry)
+
+        assets_list = []
+
+        for x in self.__mycursor__:
+            assets_list.append(x)
+
+        return assets_list
+
+'''
+    def getTransactions(self, user):
+        all_transactions = self.__mycursor__.execute(
+            f"SELECT date_time, asset, quantity, cost FROM User 
+            JOIN Transactions ON User.userID = Transactions.userID 
+            WHERE User.userID = {user};"
+        )
+        return all_transactions
+'''
