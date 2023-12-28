191f497539aac65e98d7de06d5404a0e7ee58b42
João Otávio Cano <joaootaviocano@gmail.com>
2023-12-28 14:18:04 -0300

    frontend post transaction

--- frontend/javascript/new-transaction.js
+++ frontend/javascript/new-transaction.js
@@ -60,4 +60,36 @@ function closeModal(){
     modal.style.display = '';
 }
 
+function postTransaction(){
+    const operation = document.forms.newtransaction['buy/sell'].value
+    const asset = document.forms.newtransaction.asset.value
+    const date = document.forms.newtransaction.date.value
+    const price = document.forms.newtransaction.price.value
+    const quantity = document.forms.newtransaction.quantity.value
+    const type = document.forms.newtransaction.type.value
+
+    transaction = {
+        "operation": operation,
+        "asset": asset,
+        "date": date,
+        "price": price,
+        "quantity": quantity,
+        "type": type,
+    }
+
+    const jsonString = JSON.stringify(transaction);
+
+    fetch('/newtransaction', { 
+        method: 'POST', 
+        headers:{ "authentication_key": localStorage.authenticationKey},
+        body: jsonString
+    })
+}
+
+function newTransaction(){
+    if (validateFormInputs()){
+        postTransaction();
+    }
+}
+
 listenEventRecalculateTotal()
