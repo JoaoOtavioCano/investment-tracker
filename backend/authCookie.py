aaf656e603c44948744aa93c294e410138502535
João Otávio Cano <joaootaviocano@gmail.com>
2024-01-10 18:06:01 -0300

    Adding authCookies a samesite=lax and max-age=2hours

--- backend/authCookie.py
+++ backend/authCookie.py
@@ -2,11 +2,16 @@ import http.cookies as cookies
 
 class AuthCookie():
     def __init__(self, authentication_key):
+
+        hours = 3600
+
         self.cookie = cookies.SimpleCookie()
 
         self.cookie["authenticationKey"] = authentication_key
 
         self.cookie["authenticationKey"]["httponly"] = True 
+        self.cookie["authenticationKey"]["samesite"] = 'Lax' 
+        self.cookie["authenticationKey"]["max-age"] =  2 * hours
     
     def generateHTTPheaders(self):
         return str(self.cookie).replace("Set-Cookie: ", "")
