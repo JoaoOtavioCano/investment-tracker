86f1ec54e2cebd258245e5cbeb144d29f2ef2ebe
João Otávio Cano <joaootaviocano@gmail.com>
2024-01-22 18:02:53 -0300

    frontend: forgot password validations

--- backend/forgotPassword.py
+++ backend/forgotPassword.py
@@ -15,12 +15,17 @@ class ForgotPassword:
 
         code  = self.__createCode__()
         
-        self.__saveCodeInDb__(code)
+        saved = self.__saveCodeInDb__(code)
 
-        email_manager.sendEmailToCreateNewPassword(email, code)
+        if saved:
+            email_manager.sendEmailToCreateNewPassword(email, code)
 
-        self.request.send_response(200, "OK")
-        self.request.end_headers()
+            self.request.send_response(200, "OK")
+            self.request.end_headers()
+        else:
+            self.request.send_error(500, "USER NOT FOUND")
+            self.request.end_headers()
+
 
     def __createCode__(self):
         code =  hashlib.sha256(str(random.randrange(9999999999)).encode()).hexdigest()
@@ -34,7 +39,12 @@ class ForgotPassword:
 
         user_id = db.getUserId(email)
 
+        if user_id == None:
+            return False
+
         db.deletePasswordRequest(user_id)
 
         db.addNewPasswordRequest(code, user_id)
+
+        return True
 
--- frontend/javascript/forgot-password.js
+++ frontend/javascript/forgot-password.js
@@ -11,4 +11,63 @@ function confirmEmailAddress(){
         method: 'POST',
         body: jsonString
     })
+    .then((response) => {
+        if (response.status == 500){
+            errorUserNotFound()
+        }
+    })
+}
+
+function errorMessage(field, message){
+
+    errorMessageDiv = document.getElementsByClassName(field)[0];
+    errorMessageSpan = document.getElementsByClassName(field)[0].children[0];
+    inputField = document.getElementById(field);
+
+    inputField.style.border = "2px solid red";
+
+    errorMessageDiv.style.border = "2px solid red"
+
+    errorMessageSpan.innerText = message;
+}
+
+function removeErrorMessages(){
+
+    document.getElementsByClassName("email")[0].children[0].innerText = "";
+    document.getElementsByClassName("email")[0].style.border = "0px";
+    document.getElementById("email").style.border = "0px";
+
+    document.getElementsByClassName("invalid-user")[0].style.display = "none"
+
+}
+
+function validateFormInputs(){
+    const email = document.forms["forgotpassword"]["email"].value;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    let errorOccurred = false;
+
+    if (email == ""){
+        errorMessage("email", "Must be field!");
+        errorOccurred = true;
+    }else if (!emailRegex.test(email)){
+        errorMessage("email", "Type a valid email!");
+        errorOccurred = true;
+    }
+
+    return !errorOccurred;
+}
+
+function errorUserNotFound(){
+    errorMessageDiv = document.getElementsByClassName("invalid-user")[0];
+
+    errorMessageDiv.style.display = "flex";
+}
+
+function main(){
+    removeErrorMessages();
+
+    if(validateFormInputs()){
+        confirmEmailAddress();
+    }
 }
