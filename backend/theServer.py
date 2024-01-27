b02d81a7322ced8a740d38b303ce7136fc29ac99
João Otávio Cano <joaootaviocano@gmail.com>
2024-01-27 17:32:47 -0300

    update port to int

--- backend/theServer.py
+++ backend/theServer.py
@@ -101,7 +101,7 @@ class RequestsHandler(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=RequestsHandler):
 
     print(os.getenv("PORT"))
-    server_address = ('', os.getenv("PORT"))
+    server_address = ('', int(os.getenv("PORT")))
     authenticator = Authenticator()
     httpd = server_class(server_address, lambda request, client_address, server: handler_class(request, client_address, server, authenticator))
     httpd.serve_forever()
