08cb2044a16c953d323792cfa1dcdc71bd1ff9a6
João Otávio Cano <joaootaviocano@gmail.com>
2024-01-27 18:14:04 -0300

    update

--- backend/authenticator.py
+++ backend/authenticator.py
@@ -3,12 +3,14 @@ class Authenticator:
         self.authorization_list = {}
 
     def validateAuthentication(self, request):
-        print(request.headers["Cookie"])
-        print(request.headers["Cookie"][2])
 
         authentication_key = request.headers["Cookie"].split(";")[2].replace("authenticationKey=", "")
 
+        print(authentication_key)
+
         user_id = authentication_key.split('#', 1)[0]
+
+        print(user_id)
 
         try:
             if self.authorization_list[user_id] == authentication_key:
