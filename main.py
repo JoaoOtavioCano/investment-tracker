b8e22c80430120e1af4e1cff232971355049b80b
João Otávio Cano <joaootaviocano@gmail.com>
2024-01-27 16:48:25 -0300

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 def execute_other_script():
     try:
-        subprocess.run(['python', 'theServer.py'], check=True)
+        subprocess.run(['python', 'backend/theServer.py'], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error executing other_script.py: {e}")
 
