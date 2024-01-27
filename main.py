50191f3f6ac168122a9e495504dca8c0a9c84905
João Otávio Cano <joaootaviocano@gmail.com>
2024-01-27 15:48:24 -0300

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def execute_other_script():
+    try:
+        subprocess.run(['python', 'backend/theServer.py'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing other_script.py: {e}")
+
+if __name__ == '__main__':
+    execute_other_script()
