e4f4c8b320133bcaff557a2cd2805ae2db56d795
João Otávio Cano <joaootaviocano@gmail.com>
2024-02-01 16:28:15 -0300

    no auth cookie testing

--- frontend/javascript/authentication.js
+++ frontend/javascript/authentication.js
@@ -1,6 +1,9 @@
 export function checkAuthenticationKeyExists(response){
+   console.log(response.status);
+
    if (response.status === 500){
       response.text().then((text) => {
+         console.log(text);
          if(text == 'User not authenticated'){
             let redirectPath = '/login';
       
