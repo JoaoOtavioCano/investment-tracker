aa8c729dbd250fa49cf14a3392e5c919e29491fe
João Otávio Cano <joaootaviocano@gmail.com>
2024-02-01 16:47:24 -0300

    comment everything

--- frontend/javascript/authentication.js
+++ frontend/javascript/authentication.js
@@ -1,14 +1,14 @@
 export function checkAuthenticationKeyExists(response){
 
-   if (response.status === 500){
-      response.text().then((text) => {
-         console.log(text);
-         if(text == 'User not authenticated'){
-            console.log("entrou");
-            let redirectPath = '/login';
-      
-            window.location.href = redirectPath;
-         }
-      })
-   }
+   //if (response.status === 500){
+   //   response.text().then((text) => {
+   //      console.log(text);
+   //      if(text == 'User not authenticated'){
+   //         console.log("entrou");
+   //         let redirectPath = '/login';
+   //   
+   //         window.location.href = redirectPath;
+   //      }
+   //   })
+   //}
 }
