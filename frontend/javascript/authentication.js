1cc9e90d932fb1b2e1e5b4110c475462ce0685d8
João Otávio Cano <joaootaviocano@gmail.com>
2024-02-01 16:38:58 -0300

    console.log

--- frontend/javascript/authentication.js
+++ frontend/javascript/authentication.js
@@ -1,10 +1,10 @@
 export function checkAuthenticationKeyExists(response){
-   console.log(response.status);
 
    if (response.status === 500){
       response.text().then((text) => {
          console.log(text);
          if(text == 'User not authenticated'){
+            console.log("entrou");
             let redirectPath = '/login';
       
             window.location.href = redirectPath;
