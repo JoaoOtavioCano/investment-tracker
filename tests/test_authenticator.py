e8ef10151f724c85f5d07f5da3521d0ada0fd8ee
João Otávio Cano <joaootaviocano@gmail.com>
2024-06-07 14:34:31 -0300

    tests

---
+++ tests/test_authenticator.py
@@ -0,0 +1,65 @@
+import sys
+import os
+import pytest
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from backend.authenticator import Authenticator
+
+class FakeRequest:
+    def __init__(self):
+        self.headers = {}
+
+@pytest.fixture
+def authenticator():
+    return Authenticator()
+
+def test_validateAuthentication_01(authenticator):
+    request = FakeRequest()
+    request.headers["Cookie"] = "authenticationKey=00001#99999999999;"
+    
+    authenticator.authorization_list["00001"] = "00001#99999999999"
+    
+    assert authenticator.validateAuthentication(request) == True
+
+def test_validateAuthentication_02(authenticator):
+    request = FakeRequest()
+    request.headers["Cookie"] = "authenticationKey=00002#99999999999;"
+    
+    authenticator.authorization_list["00001"] = "00001#99999999999"
+    
+    assert authenticator.validateAuthentication(request) == False
+
+def test_validateAuthentication_03(authenticator):
+    request = FakeRequest()
+    request.headers["Cookie"] = "randomCookie=123434;"
+    
+    authenticator.authorization_list["00001"] = "00001#99999999999"
+    
+    assert authenticator.validateAuthentication(request) == False
+
+def test_validateAuthentication_04(authenticator):
+    request = FakeRequest()
+    
+    authenticator.authorization_list["00001"] = "00001#99999999999"
+    
+    assert authenticator.validateAuthentication(request) == False
+
+def test_validateAuthentication_05(authenticator):
+    request = FakeRequest()
+    request.headers["Cookie"] = "authenticationKey=00001#99999999999;"
+    
+    authenticator.authorization_list["00001"] = "00001#88888888888"
+    
+    assert authenticator.validateAuthentication(request) == False
+
+def test_getUserIdAndAuthKeyFromCookies(authenticator):
+    request = FakeRequest()
+    request.headers["Cookie"] = "authenticationKey=00001#99999999999;"
+    
+    authenticator.authorization_list["00001"] = "00001#99999999999"
+    
+    assert authenticator.getUserIdAndAuthKeyFromCookies(request) == ("00001", "00001#99999999999")
+
+
