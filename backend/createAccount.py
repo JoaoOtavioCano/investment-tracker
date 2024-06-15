9c486138539a3409c34777ac33881ab82606202b
João Otávio Cano <joaootaviocano@gmail.com>
2024-06-15 18:08:24 -0300

    fix code smell

--- backend/createAccount.py
+++ backend/createAccount.py
@@ -1,6 +1,6 @@
-from .database import *
+from .database import Database
 from .payloadValidator import PayloadValidator
-from .possibleErrors import *
+from .possibleErrors import UserAlreadyExists
 
 import bcrypt
 
